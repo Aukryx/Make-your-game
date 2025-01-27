afd8d6c21e76c8f5dc21f0c5ed9be08ac35b5510
tlambert <thomaslambert9976@gmail.com>
2025-01-27 14:10:59 +0100

    added div for line and ennemies initialized

--- grid.js
+++ grid.js
@@ -1,5 +1,37 @@
 const grid = document.getElementById("grid")
-const line = document.createElement("div")
-const bloc = document.createElement("div")
-const ennemy = document.createElement("div")
 
+const bloc = document.createElement("div")
+
+
+const createLine = (lineCount) => {
+    for (let i = 1; i <= lineCount; i++) {
+        const line = document.createElement("div");
+        line.setAttribute("class", "line");
+        line.id = `line-${i}`;
+        grid.appendChild(line);
+    }
+}
+createLine(30);
+
+const createEnnemies = (ennemiesCount) => {
+    let line = 1;
+    for (let i = 1; i <= ennemiesCount; i++) {
+        const ennemy = document.createElement("div");
+        ennemy.setAttribute("class", "ennemy");
+        ennemy.id = `ennemy-${i}`;
+        
+        const currentLine = document.getElementById(`line-${line}`);
+        
+        if (currentLine) {
+            currentLine.appendChild(ennemy);
+        } else {
+            console.error(`La ligne ${line} n'existe pas`);
+        }
+        
+        if (i % 10 === 0) {
+            line++;
+        }
+    }
+}
+
+createEnnemies(50);
