15ba8c7a634e4f4a51e67b56106c077a379bf9fd
F-justine <justine.faure98@outlook.com>
2025-01-27 15:28:07 +0100

    move spaceship left and right

--- players/player.js
+++ players/player.js
@@ -1,21 +1,25 @@
+// const onePlayer = document.getElementById('spaceship');
+
 class Player {
     constructor(container) {
-        this.element = document.createElement('div');
-        this.element.className ='player';
+        // this.element = document.createElement('div');
+        // this.element.className = 'spaceship';
+        this.element = document.getElementById('spaceship');
         this.x = 400;
         this.speed = 0.5;
         this.container = container;
         this.init()
     }
 
-    init(){
-        this.element.style.left =`${this.x}px`;
-        this.container.appendChild(this.element);
+    init() {
+        this.element.style.left = `${this.x}px`;
+        // this.container.appendChild(this.element);
     }
 
     move(direction, deltaTime) {
-        this.x += direction * this.speed * deltaTime;
-        this.x = Math.max(0 , Math.min(760, this.x));
+        console.log(direction)
+        this.x += (direction * this.speed * deltaTime) * 500;
+        this.x = Math.max(0, Math.min(760, this.x));
         this.element.style.left = `${this.x}px`
     }
 }
@@ -24,14 +28,41 @@ const grid = document.getElementById('grid');
 const player = new Player(grid)
 
 let lastTime = 0;
-function gameLoop(timestamp){
+let direction = 0;
+
+function gameLoop(timestamp) {
     const deltaTime = (timestamp - lastTime) / 1000;
     lastTime = timestamp;
 
-    player.move(1, deltaTime);
+//   const test = document.getElementsByClassName('direction_space')[0]
+//     test.textContent = `${direction} / ${player.x}`
+    player.move(direction, deltaTime);
 
     requestAnimationFrame(gameLoop)
 }
 
 requestAnimationFrame(gameLoop)
 
+
+window.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowRight') {
+        direction = 1
+    } else if (event.key === 'ArrowLeft') {
+        direction = -1
+    }
+});
+
+window.addEventListener('keyup', () => {
+    direction = 0
+});
+
+// function updatePlayerMovement(timestamp){
+//     const deltaTime = (timestamp - lastTime) / 1000;
+//     lastTime = timestamp
+
+//     player.move(direction, deltaTime);
+
+//     requestAnimationFrame(updatePlayerMovement)
+// }
+
+// requestAnimationFrame(updatePlayerMovement)
